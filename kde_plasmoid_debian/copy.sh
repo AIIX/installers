a255b46245d31786e45e13df9bc9dc099b849cb9
Aditya Mehra <Aix.m@outlook.com>
2018-04-24 06:06:42 +0930

    update copy

--- kde_plasmoid_debian/copy.sh
+++ kde_plasmoid_debian/copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /kde_plasmoid_debian/dependencies.sh ~/mycroft-core/
-cp /kde_plasmoid_debian/installmycroft.sh ~/mycroft-core/
+cp tmp/installers/kde_plasmoid_debian/dependencies.sh ~/mycroft-core/
+cp tmp/installers/kde_plasmoid_debian/installmycroft.sh ~/mycroft-core/
 cd ~
 cd mycroft-core
 chmod a+x dependencies.sh
