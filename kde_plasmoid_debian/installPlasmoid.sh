60d67e186af581b3a38ab1acfcbb6d4579c23e52
Aditya Mehra <Aix.m@outlook.com>
2018-05-03 07:52:10 +0930

    exp

--- kde_plasmoid_debian/installPlasmoid.sh
+++ kde_plasmoid_debian/installPlasmoid.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -Ee
 
-getuser=$(who am i | awk '{print $1}')
-echo $getuser
-
 # Build the Plasmoid
-#cd /home/$getuser/plasma-mycroft
-#mkdir build
-#cd build
-#cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
-#make
+cd ~
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
+make
 
 # Install the Plasmoid
-#make install
+make install
 
 # Set permissions on new files to allow execution
-#pkexec /tmp/installers/setpermissions.sh
+pkexec /tmp/installers/setpermissions.sh
