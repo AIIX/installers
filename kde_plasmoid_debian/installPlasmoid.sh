ea94ddf4febc92073a273d585d17a85bafff14f4
Aditya Mehra <Aix.m@outlook.com>
2018-05-03 07:59:53 +0930

    exp

--- kde_plasmoid_debian/installPlasmoid.sh
+++ kde_plasmoid_debian/installPlasmoid.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -Ee
-
+pstree -lu -s $$ | grep --max-count=1 -o '([^)]*)' | head -n 1
 # Build the Plasmoid
 cd ~
+cd plasma-mycroft
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DKDE_INSTALL_LIBDIR=lib -DKDE_INSTALL_USE_QT_SYS_PATHS=ON
