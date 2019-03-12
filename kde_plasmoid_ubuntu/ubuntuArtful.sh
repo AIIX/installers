cee32a57244dded08e77bd86c17255cf4622eecf
Aditya Mehra <Aix.m@outlook.com>
2019-03-12 10:26:23 +0530

    Update Mycroft Deps

--- kde_plasmoid_ubuntu/ubuntuArtful.sh
+++ kde_plasmoid_ubuntu/ubuntuArtful.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y git python python-dev python-setuptools python-gobject-2-dev libtool libffi-dev libssl-dev autoconf automake bison swig libglib2.0-dev portaudio19-dev mpg123 screen flac curl libicu-dev pkg-config automake libjpeg-dev libfann-dev build-essential jq psmisc
+sudo apt-get install -y git python3 python3-dev python-setuptools python-gobject-2-dev libtool libffi-dev libssl-dev autoconf automake bison swig libglib2.0-dev portaudio19-dev mpg123 screen flac curl libicu-dev pkg-config automake libjpeg-dev libfann-dev build-essential jq psmisc
+
